eb0a2c5eea471c97b127bde1030eccc78869373c
thiag8ito <thiagoryoitoprog@gmail.com>
2026-06-11 16:03:11 -0300

    commit

---
+++ cap09/mini-projeto/main.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+np.random.seed(42)
+
+data = {
+    'ID_Pedido': range(1001, 1101),
+    'Data_Compra': pd.to_datetime(pd.date_range(start = '2026-07-01', periods = 100, freq = 'D')) - pd.to_timedelta(np.random.randint(0, 30, size=100), unit='d'),
+    'Cliente_ID': np.random.randint(100, 150, size = 100),
+    'Produto': np.random.choice(['Smartphone', 'Notebook', 'Fone de Ouvido', 'Smatwatch', 'Teclado Mecanico'], size=100),
+    'Categoria': ['Eletrônicos', 'Eletrônicos', 'Acessórios', 'Acessórios', 'Acessórios'] * 20,
+    'Quantidade': np.random.randint(1, 5, size = 100),
+    'Preco_Unitario': [5999.90, 8500.00, 799.50, 2100.00, 850.00] * 20,
+    'Status_Entrega': np.random.choice(['Entregue', 'Pendente', 'Cancelado'], size=100, p=[0.8, 0.15, 0.05])
+}
+
+df_vendas = pd.DataFrame(data)
